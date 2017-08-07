e0ac9c022faa5f29882acf5bc73c8683e4e310c7
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-06 23:13:24 -0300

    Finalização do KafkaService com close().

--- src/main/java/com/infobarbosa/App.java
+++ src/main/java/com/infobarbosa/App.java
@@ -4,8 +4,12 @@ import com.infobarbosa.KafkaService;
 
 public class App {
     public static void main( String[] args ){
-
-    	KafkaService kafka = KafkaService.getInstance();
-    	kafka.pool();
+		KafkaService kafka = KafkaService.getInstance();
+	    	
+    	try{
+	    	kafka.poll();
+	    }finally{
+	    	kafka.close();
+	    }
     }
 }
